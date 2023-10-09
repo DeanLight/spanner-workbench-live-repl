5212c1d61dc44c7972ba4ead3d45cad82fe383b2
loayshaqir1 <loayshaqir0@gmail.com>
2023-10-09 23:58:03 +0300

    dont register rust and nlp ie funcs

--- refactor_files.py
+++ refactor_files.py
@@ -1,12 +1,33 @@
 import os
 
 supported_ie_funcs = "PREDEFINED_IE_FUNCS = [PYRGX, PYRGX_STRING, JsonPath, JsonPathFull]"
+dirname = os.path.dirname(__file__)
+def remove_predefined_ie_funcs(file_path):
+    with open(file_path, 'r') as file:
+        file_content = file.readlines()
+
+    start_index = None
+    end_index = None
+    for i, line in enumerate(file_content):
+        if 'PREDEFINED_IE_FUNCS' in line and start_index is None:
+            start_index = i
+            print(start_index)
+        elif start_index is not None and ']' in line:
+            end_index = i
+            print(end_index)
+            break
+
+    if start_index is not None and end_index is not None:
+        del file_content[start_index:end_index + 1]
+
+    with open(file_path, 'w') as file:
+        file.writelines(file_content)
 
 def refactor_files():
     """
     A function that removes all imports of fastcore or nbdev libraries as they include unsupported libraries
     """
-    for foldername, subfolders, filenames in os.walk(os.path.join(os.path.dirname(__file__),'content')):
+    for foldername, subfolders, filenames in os.walk(os.path.join(dirname,'content')):
         for filename in filenames:
             if filename.endswith('.py'):
                 file_path = os.path.join(foldername, filename)
@@ -25,4 +46,5 @@ def refactor_files():
                             file.write(line)
 
 if __name__ == '__main__':
+    remove_predefined_ie_funcs(os.path.join(dirname,'content','rgxlog','session.py'))
     refactor_files()
